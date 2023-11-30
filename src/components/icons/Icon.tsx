bf18b026dffedb14a405e87e2f8df613aac5cb86
hamo-o <yheel369@gmail.com>
2023-11-30 11:17:24 +0900

    feat: 아이콘 기본설정

---
+++ src/components/icons/Icon.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+import * as icons from "@/components/icons/svgs";
+import { KeyOfColor, TypeOfColor, colors } from "@/styles/theme";
+
+interface Props {
+  icon: keyof typeof icons;
+  width?: number;
+  height?: number;
+  margin?: string;
+  style?: any;
+
+  rotate?: number;
+  fill?: KeyOfColor;
+  color?: KeyOfColor;
+
+  onClick?: any;
+  onTouchEnd?: any;
+  selected?: boolean;
+}
+
+const Icon = ({
+  icon,
+  width,
+  height,
+  margin,
+  style,
+
+  rotate,
+  fill,
+  color,
+
+  onClick,
+  onTouchEnd,
+  selected,
+}: Props) => {
+  const IconComponent = icons[icon];
+
+  return (
+    <IconWrapper
+      onClick={onClick}
+      onTouchEnd={onTouchEnd}
+      style={{ width: width, height: height, margin: margin }}
+      rotate={rotate}
+      fill={fill}
+      color={color}
+      styles={style}
+    >
+      <IconComponent />
+    </IconWrapper>
+  );
+};
+
+const IconWrapper = styled.div<{
+  width?: number;
+  height?: number;
+  rotate?: number;
+  fill?: KeyOfColor;
+  color?: KeyOfColor;
+  styles?: any;
+}>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: ${({ width }) => width}px;
+  height: ${({ height }) => height}px;
+
+  ${({ rotate }) =>
+    rotate && {
+      transform: `rotate(${rotate}deg)`,
+    }}
+  svg {
+    width: ${({ width }) => width}px;
+    height: ${({ height }) => height}px;
+
+    path {
+      stroke: ${({ color }) => (color ? colors[color] : "transparent")};
+      fill: ${({ fill }) => (fill ? colors[fill] : "transparent")};
+    }
+  }
+
+  ${({ styles }) => styles}
+`;
+
+export default Icon;
