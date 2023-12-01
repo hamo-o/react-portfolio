7f0a85b724800039dae8ca1891fba22454419baa
hamo-o <yheel369@gmail.com>
2023-12-01 16:40:19 +0900

    feat: 디테일 페이지 이동 시 커서 reset

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,9 +1,23 @@
 "use client";
 
+import { useEffect } from "react";
+import { useResetRecoilState } from "recoil";
+import { cursorState } from "@/utils/atom";
+
 import Flex from "@/components/common/Flex";
 
 const ProjectPage = ({ params }: { params: { id: number } }) => {
-  return <Flex>프로젝트 디테일 {params.id}페이지입니다.</Flex>;
+  const resetCursor = useResetRecoilState(cursorState);
+
+  useEffect(() => {
+    resetCursor();
+  }, []);
+
+  return (
+    <>
+      <Flex>프로젝트 디테일 {params.id}페이지입니다.</Flex>
+    </>
+  );
 };
 
 export default ProjectPage;
