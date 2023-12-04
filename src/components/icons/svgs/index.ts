7a7608eb219735df15dee8eb326253af74313d3f
hamo-o <yheel369@gmail.com>
2023-12-04 19:53:34 +0900

    feat: ArrowRight svg 추가

--- src/components/icons/svgs/index.ts
+++ src/components/icons/svgs/index.ts
@@ -2,3 +2,4 @@ export { default as Star } from "./Star";
 export { default as Eye } from "./Eye";
 export { default as Eyeball } from "./Eyeball";
 export { default as Eyebrow } from "./Eyebrow";
+export { default as ArrowRight } from "./ArrowRight";
