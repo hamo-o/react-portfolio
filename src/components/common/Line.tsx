9b3ba7ce4a8d7cd14623f79e5eb378e0a88ef95d
hamo-o <yheel369@gmail.com>
2023-12-06 18:10:40 +0900

    feat: Line 컴포넌트

---
+++ src/components/common/Line.tsx
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+import { KeyOfColor, colors } from "@/styles/theme";
+
+const Line = styled.div<{
+  width?: number | string;
+  height?: number | string;
+  color: KeyOfColor;
+}>`
+  width: ${({ width }) =>
+    typeof width === "string" ? width : width ? `${width}px` : "100%"};
+  height: ${({ height }) =>
+    typeof height === "string" ? height : height ? `${height}px` : "auto"};
+  background: ${({ color }) => colors[color]};
+`;
+
+export default Line;
