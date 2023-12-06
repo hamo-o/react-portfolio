c4dd9257ad9641c49a4c7658abf5feae23b39433
hamo-o <yheel369@gmail.com>
2023-12-06 11:13:24 +0900

    feat: 이미지 썸네일 파일 및 필드 추가

--- src/models/project.ts
+++ src/models/project.ts
@@ -3,6 +3,7 @@ export interface Project {
   name: string;
   date: string;
   stack: string[];
+  thumnail: string;
 }
 
 export const PROJECT = [
@@ -18,30 +19,35 @@ export const PROJECT = [
       "FRAMER-MOTION",
       "GITHUB ACTIONS",
     ],
+    thumnail: "qfeed_main.png",
   },
   {
     id: 2,
     name: "WALDREG",
     date: "2023. 01. 06 ~ ",
     stack: ["REACT", "TYPESCRIPT", "REACT-QUERY"],
+    thumnail: "waldreg_main.png",
   },
   {
     id: 3,
     name: "RECIPEASY",
     date: "2022. 10. 07 ~ 2023. 01. 28",
     stack: ["NEXT.JS", "TYPESCRIPT", "TAILWIND CSS", "GITHUB ACTIONS", "PWA"],
+    thumnail: "recipeasy_main.png",
   },
   {
     id: 4,
     name: "WELCOME TO THE DMZ",
     date: "2022. 11. 04 ~ 2022. 12. 18",
     stack: ["REACT", "THREE JS"],
+    thumnail: "dmz_main.png",
   },
   {
     id: 5,
     name: "GROUPING",
     date: "2022. 08. 02 ~ 2022. 08. 23",
     stack: ["DJANGO", "VANILA JS"],
+    thumnail: "grouping_main.png",
   },
 ];
 Object.freeze(PROJECT);
