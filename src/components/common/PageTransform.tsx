d8a946b4117d55a1d36bd3deabb2de7a44b8f68a
hamo-o <yheel369@gmail.com>
2023-12-07 16:04:26 +0900

    feat: 페이지 전환 컴포넌트

---
+++ src/components/common/PageTransform.tsx
@@ -0,0 +1,42 @@
+import styled from "@emotion/styled";
+import { motion } from "framer-motion";
+
+import Flex from "./Flex";
+import { colors } from "@/styles/theme";
+
+interface Position {
+  x: number;
+  y: number;
+}
+
+const PageTransform = ({ position }: { position: Position }) => {
+  return (
+    <PageBackground width="100vw" height="100vh">
+      <MotionCircle
+        initial={{ width: "4rem", height: "4rem", borderWidth: "20rem" }}
+        animate={{ width: "200vw", height: "200vw", borderWidth: "1rem" }}
+        transition={{ duration: 1, type: "tween" }}
+        style={{
+          position: "absolute",
+          top: `calc(${position.y} - 200vw)`,
+          left: `calc(${position.x} - 200vw)`,
+        }}
+      />
+    </PageBackground>
+  );
+};
+
+const PageBackground = styled(Flex)`
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  z-index: 10000;
+`;
+
+const MotionCircle = styled(motion.div)`
+  border-radius: 50%;
+  border: 1rem solid ${colors.primary_purple};
+`;
+
+export default PageTransform;
