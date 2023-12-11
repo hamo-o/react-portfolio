52919d588ee7799fa395f8971ccf29daa8704dd6
hamo-o <yheel369@gmail.com>
2023-12-11 18:38:28 +0900

    design: font 크기 시각보정

--- src/styles/theme/typo.tsx
+++ src/styles/theme/typo.tsx
@@ -7,7 +7,7 @@ export const typo = {
     font-family: "NATS";
     font-style: normal;
     font-weight: 400;
-    font-size: ${calcRem(88)};
+    font-size: ${calcRem(80)};
     letter-spacing: "-1.6%";
     line-height: 0.75;
   `,
@@ -15,7 +15,7 @@ export const typo = {
     font-family: "pretendard";
     font-style: normal;
     font-weight: 600;
-    font-size: ${calcRem(30)};
+    font-size: ${calcRem(28)};
     letter-spacing: -0.4px;
     line-height: 140%;
   `,
@@ -23,7 +23,7 @@ export const typo = {
     font-family: "NATS";
     font-style: normal;
     font-weight: 400;
-    font-size: ${calcRem(40)};
+    font-size: ${calcRem(36)};
     line-height: "120%";
     letter-spacing: "-1.6%";
   `,
@@ -31,7 +31,7 @@ export const typo = {
     font-family: "pretendard";
     font-style: normal;
     font-weight: 500;
-    font-size: ${calcRem(22)};
+    font-size: ${calcRem(20)};
     letter-spacing: -0.4px;
     line-height: 180%;
   `,
@@ -39,7 +39,7 @@ export const typo = {
     font-family: "NATS";
     font-style: normal;
     font-weight: 500;
-    font-size: ${calcRem(28)};
+    font-size: ${calcRem(24)};
     letter-spacing: -0.4px;
     line-height: 180%;
   `,
@@ -47,19 +47,19 @@ export const typo = {
     font-family: "Zen Dots";
     font-style: normal;
     font-weight: 400;
-    font-size: ${calcRem(24)};
+    font-size: ${calcRem(20)};
   `,
   number2: css`
     font-family: "Zen Dots";
     font-style: normal;
     font-weight: 400;
-    font-size: ${calcRem(14)};
+    font-size: ${calcRem(12)};
   `,
   detail: css`
     font-family: "pretendard";
     font-style: normal;
     font-weight: 500;
-    font-size: ${calcRem(17)};
+    font-size: ${calcRem(14)};
     letter-spacing: -0.4px;
   `,
 } as const;
