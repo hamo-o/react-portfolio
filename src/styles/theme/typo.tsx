2b91a4af671e81b841198c600c22b7bf2cdd0924
hamo-o <yheel369@gmail.com>
2023-12-12 07:13:54 +0900

    design: 폰트 크기 조정

--- src/styles/theme/typo.tsx
+++ src/styles/theme/typo.tsx
@@ -1,42 +1,42 @@
-import { css } from "@emotion/react";
+import { css } from '@emotion/react';
 
 export const calcRem = (px: number) => `${px / 16}rem`;
 
 export const typo = {
   title1: css`
-    font-family: "NATS";
+    font-family: 'NATS';
     font-style: normal;
     font-weight: 400;
     font-size: ${calcRem(80)};
-    letter-spacing: "-1.6%";
+    letter-spacing: '-1.6%';
     line-height: 0.75;
   `,
   subtitle1: css`
-    font-family: "pretendard";
+    font-family: 'pretendard';
     font-style: normal;
     font-weight: 600;
-    font-size: ${calcRem(28)};
+    font-size: ${calcRem(24)};
     letter-spacing: -0.4px;
     line-height: 140%;
   `,
   body1: css`
-    font-family: "NATS";
+    font-family: 'NATS';
     font-style: normal;
     font-weight: 400;
     font-size: ${calcRem(36)};
-    line-height: "120%";
-    letter-spacing: "-1.6%";
+    line-height: '120%';
+    letter-spacing: '-1.6%';
   `,
   body2: css`
-    font-family: "pretendard";
+    font-family: 'pretendard';
     font-style: normal;
     font-weight: 500;
-    font-size: ${calcRem(20)};
+    font-size: ${calcRem(18)};
     letter-spacing: -0.4px;
     line-height: 180%;
   `,
   body3: css`
-    font-family: "NATS";
+    font-family: 'NATS';
     font-style: normal;
     font-weight: 500;
     font-size: ${calcRem(24)};
@@ -44,19 +44,19 @@ export const typo = {
     line-height: 180%;
   `,
   number: css`
-    font-family: "Zen Dots";
+    font-family: 'Zen Dots';
     font-style: normal;
     font-weight: 400;
     font-size: ${calcRem(20)};
   `,
   number2: css`
-    font-family: "Zen Dots";
+    font-family: 'Zen Dots';
     font-style: normal;
     font-weight: 400;
     font-size: ${calcRem(11)};
   `,
   detail: css`
-    font-family: "pretendard";
+    font-family: 'pretendard';
     font-style: normal;
     font-weight: 500;
     font-size: ${calcRem(14)};
