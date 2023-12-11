512c516b20abb880da0a01614b119406ded9db85
hamo-o <yheel369@gmail.com>
2023-12-12 07:01:36 +0900

    feat: 프로젝트 수행 역할 추가

--- src/components/cards/CardMedium.tsx
+++ src/components/cards/CardMedium.tsx
@@ -1,17 +1,18 @@
-import { useState } from "react";
-import styled from "@emotion/styled";
-import { motion } from "framer-motion";
-import { useSetRecoilState } from "recoil";
+import { useState } from 'react';
+import styled from '@emotion/styled';
+import { motion } from 'framer-motion';
+import { useSetRecoilState } from 'recoil';
 
-import { cursorState } from "@/utils/atom";
+import { cursorState } from '@/utils/atom';
 
-import Flex from "../common/Flex";
-import Text from "../common/Text";
-import NextImage from "../common/NextImage";
-import Icon from "../icons/Icon";
+import Flex from '../common/Flex';
+import Text from '../common/Text';
+import NextImage from '../common/NextImage';
+import Icon from '../icons/Icon';
+import TagSmall from '../tags/TagSmall';
 
-import { calcRem, colors } from "@/styles/theme";
-import { Project } from "@/models/project";
+import { calcRem, colors } from '@/styles/theme';
+import { Project } from '@/models/project';
 
 const CardMedium = ({
   project,
@@ -24,11 +25,12 @@ const CardMedium = ({
   const [isOpen, setIsOpen] = useState(false);
 
   const projectEnter = () => {
-    setCursorVariant("project");
+    setCursorVariant('project');
   };
 
   const projectLeave = () => {
-    setCursorVariant("default");
+    setCursorVariant('default');
+    setIsOpen(false);
   };
 
   return (
@@ -43,42 +45,82 @@ const CardMedium = ({
         layout
         onClick={onClick}
         style={{
-          width: isOpen ? "94vw" : "auto",
-          height: isOpen ? "90vh" : "64vh",
-          padding: isOpen ? "6rem" : "0",
-          position: isOpen ? "fixed" : "static",
-          zIndex: isOpen ? "1000" : "0",
-          gap: isOpen ? "6rem" : "0",
-          background: isOpen ? colors.primary_purple : "none",
+          width: isOpen ? '94vw' : 'auto',
+          height: isOpen ? '90vh' : '64vh',
+          padding: isOpen ? '6rem' : '0',
+          position: isOpen ? 'fixed' : 'static',
+          zIndex: isOpen ? '1000' : '0',
+          gap: isOpen ? '6rem' : '0',
+          background: isOpen ? colors.primary_purple : 'none',
         }}
       >
         <Flex
           direction="column"
-          width={isOpen ? "fit-content" : "100%"}
-          justify={isOpen ? "start" : "space-between"}
+          width={isOpen ? 'fit-content' : '100%'}
+          justify={isOpen ? 'start' : 'space-between'}
           gap={isOpen ? 60 : 0}
         >
           <Flex direction="column" align="left" gap={20}>
             <Icon icon="Star" fill="primary_yellow" width={65} />
             <Text typo="title1">{project.name}</Text>
-            <Text typo="number" style={{ whiteSpace: "nowrap" }}>
+            <Text typo="number" style={{ whiteSpace: 'nowrap' }}>
               {project.date}
             </Text>
           </Flex>
-          <CardContent direction="column" align="left" gap={18}>
-            <Flex direction="column" align="left" gap={8}>
-              {project.stack.map((stack: string, idx: number) => (
-                <Text key={idx} typo="body1" color="primary_black_60">
-                  {stack}
+          <CardContent direction="column" align="start" gap={40}>
+            <Flex direction="column" align="left" gap={16}>
+              {isOpen && (
+                <Text typo="body2" color="primary_black">
+                  사용 기술
                 </Text>
-              ))}
+              )}
+              <Flex
+                direction={isOpen ? 'row' : 'column'}
+                align="start"
+                justify="start"
+                gap={8}
+                style={{ flexWrap: 'wrap' }}
+              >
+                {project.stack.map((stack: string, idx: number) =>
+                  isOpen ? (
+                    <TagSmall
+                      key={idx}
+                      content={stack}
+                      color="primary_black_60"
+                      background="primary_white_30"
+                    />
+                  ) : (
+                    <Text
+                      key={idx}
+                      typo={isOpen ? 'body3' : 'body1'}
+                      color="primary_black_60"
+                    >
+                      {stack}
+                    </Text>
+                  )
+                )}
+              </Flex>
             </Flex>
+            {isOpen && project.role && (
+              <Flex
+                width="max-content"
+                direction="column"
+                align="left"
+                gap={16}
+              >
+                <Text typo="body2" color="primary_black">
+                  수행 역할
+                </Text>
+                <Flex direction="column" align="left" gap={4}>
+                  {project.role.map((role: string, idx: number) => (
+                    <Text key={idx} typo="body2" color="primary_black_60">
+                      • {role}
+                    </Text>
+                  ))}
+                </Flex>
+              </Flex>
+            )}
           </CardContent>
-          {isOpen && (
-            <Flex direction="column" align="left" gap={8}>
-              <Text typo="body1" color="primary_white"></Text>
-            </Flex>
-          )}
         </Flex>
         {isOpen && (
           <ImageWrapper>
@@ -125,7 +167,9 @@ const CardDetail = styled(motion.div)`
   left: 3vw;
 `;
 
-const CardContent = styled(Flex)``;
+const CardContent = styled(Flex)`
+  max-width: 50vw;
+`;
 
 const ImageWrapper = styled.div`
   width: 100%;
--- src/models/project.ts
+++ src/models/project.ts
@@ -4,50 +4,74 @@ export interface Project {
   date: string;
   stack: string[];
   thumnail: string;
+  role?: string[];
 }
 
 export const PROJECT = [
   {
     id: 1,
-    name: "QFEED",
-    date: "2023. 06. 23 ~",
+    name: 'QFEED',
+    date: '2023. 06. 23 ~',
     stack: [
-      "NEXT.JS",
-      "REACT-NATIVE",
-      "TYPESCRIPT",
-      "REACT-QUERY",
-      "FRAMER-MOTION",
-      "GITHUB ACTIONS",
+      'NEXT.JS',
+      'REACT-NATIVE',
+      'TYPESCRIPT',
+      'REACT-QUERY',
+      'FRAMER-MOTION',
+      'GITHUB ACTIONS',
     ],
-    thumnail: "qfeed_main.png",
+    thumnail: 'qfeed_main.png',
+    role: [
+      '카카오 로그인, 회원가입',
+      '질문 생성 타이머 로직, 검색, 마이페이지',
+      'Presigned URL 활용 사진 업로드',
+      'RN 영역 구현(스플래시, 뒤로가기, 로딩 등), 앱 배포',
+      'CI/CD 구축 및 브랜치 관리',
+    ],
   },
   {
     id: 2,
-    name: "WALDREG",
-    date: "2023. 01. 06 ~ ",
-    stack: ["REACT", "TYPESCRIPT", "REACT-QUERY", "DOCKER"],
-    thumnail: "waldreg_main.png",
+    name: 'WALDREG',
+    date: '2023. 01. 06 ~ ',
+    stack: ['REACT', 'TYPESCRIPT', 'REACT-QUERY', 'DOCKER'],
+    thumnail: 'waldreg_main.png',
+    role: [
+      '역할 설정, 유저 관리, 상벌점 페이지',
+      '네비게이션',
+      '도커 이미지 제작',
+      'API 문서 논의 및 FE 리딩',
+    ],
   },
   {
     id: 3,
-    name: "RECIPEASY",
-    date: "2022. 10. 07 ~ 2023. 01. 28",
-    stack: ["NEXT.JS", "TYPESCRIPT", "TAILWIND CSS", "GITHUB ACTIONS", "PWA"],
-    thumnail: "recipeasy_main.png",
+    name: 'RECIPEASY',
+    date: '2022. 10. 07 ~ 2023. 01. 28',
+    stack: ['NEXT.JS', 'TYPESCRIPT', 'TAILWIND CSS', 'GITHUB ACTIONS', 'PWA'],
+    thumnail: 'recipeasy_main.png',
+    role: [
+      '카카오 로그인',
+      '닉네임 설정, 마이페이지, 레시피 상세페이지, 검색',
+      'PWA 배포',
+    ],
   },
   {
     id: 4,
-    name: "WELCOME TO THE DMZ",
-    date: "2022. 11. 04 ~ 2022. 12. 18",
-    stack: ["REACT", "THREE JS"],
-    thumnail: "dmz_main.png",
+    name: 'WELCOME TO THE DMZ',
+    date: '2022. 11. 04 ~ 2022. 12. 18',
+    stack: ['REACT', 'THREE JS'],
+    thumnail: 'dmz_main.png',
   },
   {
     id: 5,
-    name: "GROUPING",
-    date: "2022. 08. 02 ~ 2022. 08. 23",
-    stack: ["DJANGO", "VANILA JS"],
-    thumnail: "grouping_main.png",
+    name: 'GROUPING',
+    date: '2022. 08. 02 ~ 2022. 08. 23',
+    stack: ['DJANGO', 'VANILA JS'],
+    thumnail: 'grouping_main.png',
+    role: [
+      '그룹 페이지, 약속 페이지, 메인페이지',
+      '시간표 로직',
+      'tagify 적용',
+    ],
   },
 ];
 Object.freeze(PROJECT);
