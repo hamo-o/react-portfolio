99913321fdd20ca77fa9d3057d9aa82d7cd2d845
hamo-o <yheel369@gmail.com>
2023-12-11 15:57:15 +0900

    design: next image UI 변경

--- src/components/common/NextImage.tsx
+++ src/components/common/NextImage.tsx
@@ -1,32 +1,54 @@
 import Image from "next/image";
 import styled from "@emotion/styled";
 
+import Flex from "./Flex";
+
 interface ImageProp {
   src: string;
   alt: string;
+  width?: number | string;
+  height?: number | string;
   borderRadius?: string;
+  objectFit?: "cover" | "contain";
 }
 
 const NextImage = (image: ImageProp) => {
   return (
     <ImageContianer
+      width={
+        image.width
+          ? typeof image.width === "number"
+            ? `${image.width}px`
+            : image.width
+          : "100%"
+      }
+      height={
+        image.height
+          ? typeof image.height === "number"
+            ? `${image.height}px`
+            : image.height
+          : "100%"
+      }
       style={{ borderRadius: image.borderRadius ? image.borderRadius : 0 }}
     >
-      <ImageContent src={image.src} alt={image.alt} fill sizes="100vw" />
+      <ImageContent
+        src={image.src}
+        alt={image.alt}
+        fill
+        sizes="100vw"
+        objectFit={image.objectFit ? image.objectFit : "cover"}
+      />
     </ImageContianer>
   );
 };
 
-const ImageContianer = styled.div`
-  width: 100%;
-  height: 100%;
-
+const ImageContianer = styled(Flex)`
   overflow: hidden;
   position: relative;
 `;
 
-const ImageContent = styled(Image)`
-  object-fit: cover;
+const ImageContent = styled(Image)<{ objectFit: "cover" | "contain" }>`
+  object-fit: ${({ objectFit }) => objectFit};
 `;
 
 export default NextImage;
