9eb14d48623ff82787de6d424ae36e0afa583ea1
hamo-o <yheel369@gmail.com>
2023-12-14 11:02:12 +0900

    feat: Contact 컴포넌트 반응형 적용

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -5,17 +5,22 @@ import { motion } from "framer-motion";
 import Icon from "../icons/Icon";
 import Flex from "../common/Flex";
 import Text from "../common/Text";
+import { media } from "@/styles/theme";
 
 const Contact = () => {
   return (
-    <Flex gap={50}>
+    <Icons>
       <IconWrapper
         initial={{ opacity: 0.6 }}
         whileHover={{ opacity: 1, scale: 1.2 }}
       >
         <LinkWrapper href="https://github.com/hamo-o">
           <Icon icon="Github" width={40} height={40} fill="primary_white" />
-          <Text typo="body3" color="primary_white">
+          <Text
+            typo="body3"
+            color="primary_white"
+            style={{ whiteSpace: "nowrap" }}
+          >
             hamo-o
           </Text>
         </LinkWrapper>
@@ -29,20 +34,36 @@ const Contact = () => {
           yheel6@naver.com
         </Text>
       </IconWrapper>
-    </Flex>
+    </Icons>
   );
 };
+
+const Icons = styled(Flex)`
+  gap: 50px;
+
+  ${media.mobile} {
+    gap: 2rem;
+  }
+`;
 
 const LinkWrapper = styled(Link)`
   display: flex;
   align-items: center;
   gap: 1rem;
+
+  ${media.mobile} {
+    gap: 0.5rem;
+  }
 `;
 
 const IconWrapper = styled(motion.div)`
   display: flex;
   align-items: center;
   gap: 1rem;
+
+  ${media.mobile} {
+    gap: 0.5rem;
+  }
 
   cursor: pointer;
 `;
