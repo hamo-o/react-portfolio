8b9a3c0e7a2c886634560e272da746394db09e25
hamo-o <yheel369@gmail.com>
2024-01-12 23:42:49 +0900

    fix: PC에서도 cancel button 활성화

--- src/components/cards/CardMedium.tsx
+++ src/components/cards/CardMedium.tsx
@@ -33,8 +33,8 @@ const CardMedium = ({
   const setCursorVariant = useSetRecoilState(cursorState);
   const [isOpen, setIsOpen] = useState(false);
   const wrapperRef = useRef<HTMLButtonElement>(null);
-  const textRef = useRef<HTMLDivElement>(null);
-  const lastRef = useRef<HTMLButtonElement>(null);
+  const firstRef = useRef<HTMLButtonElement>(null);
+  const lastRef = useRef<HTMLDivElement>(null);
 
   const projectEnter = () => {
     setCursorVariant('project');
@@ -46,9 +46,25 @@ const CardMedium = ({
 
   const window = useWindowSize();
 
+  const handleFocusModal = (e: any) => {
+    if (e.keyCode === 9 && isOpen) {
+      if (!e.shiftKey) {
+        if (e.target === lastRef.current) {
+          e.preventDefault();
+          firstRef.current && firstRef.current.focus();
+        }
+      } else {
+        if (e.target === firstRef.current) {
+          e.preventDefault();
+          lastRef.current && lastRef.current.focus();
+        }
+      }
+    }
+  };
+
   useEffect(() => {
-    if (lastRef.current && isOpen) {
-      lastRef.current.focus();
+    if (firstRef.current && isOpen) {
+      firstRef.current.focus();
     }
   }, [isOpen]);
 
@@ -58,9 +74,11 @@ const CardMedium = ({
         onClick={() => setIsOpen(!isOpen)}
         onMouseEnter={projectEnter}
         onMouseLeave={projectLeave}
+        onKeyDown={handleFocusModal}
         ref={wrapperRef}
         aria-haspopup="true"
         aria-expanded={isOpen}
+        tabIndex={isOpen ? -1 : 0}
         layout
         style={{
           width: isOpen || window.width < MOBILE ? '100%' : 'auto',
@@ -71,171 +89,174 @@ const CardMedium = ({
           position: isOpen ? 'fixed' : 'static',
           zIndex: isOpen ? '1000' : '0',
 
-          flexDirection: window.width >= MOBILE ? 'row' : 'column',
-
           gap: isOpen ? (window.width >= MOBILE ? '2.5rem' : '1.5rem') : '0',
         }}
       >
-        <Flex
-          direction="column"
-          width={isOpen && window.width >= MOBILE ? 'max-content' : '100%'}
+        <Flex justify="space-between">
+          <Icon
+            icon="Star"
+            fill="primary_yellow"
+            width={window.width >= MOBILE ? 60 : 35}
+            height={window.width >= MOBILE ? 60 : 35}
+          />
+          {isOpen && (
+            <button ref={firstRef} tabIndex={1}>
+              <Icon
+                icon="Cancel"
+                width={20}
+                height={20}
+                fill="primary_white_60"
+                onClick={() => setIsOpen(false)}
+                readable
+              />
+            </button>
+          )}
+        </Flex>
+        <CardContainer
           height="100%"
-          justify={isOpen ? 'start' : 'space-between'}
-          gap={isOpen || window.width < MOBILE ? 40 : 0}
+          direction={window.width >= MOBILE ? 'row' : 'column'}
+          justify="space-between"
+          gap={32}
         >
           <Flex
             direction="column"
-            align="start"
-            gap={window.width >= MOBILE ? 20 : 10}
-            style={{ padding: isOpen ? '0 1rem' : '0' }}
-          >
-            <Flex justify="space-between">
-              <Icon
-                icon="Star"
-                fill="primary_yellow"
-                width={window.width >= MOBILE ? 60 : 35}
-                height={window.width >= MOBILE ? 60 : 35}
-              />
-              {isOpen && window.width < MOBILE && (
-                <button ref={lastRef} tabIndex={1}>
-                  <Icon
-                    icon="Cancel"
-                    width={20}
-                    height={20}
-                    fill="primary_white_60"
-                    onClick={() => setIsOpen(false)}
-                    readable
-                  />
-                </button>
-              )}
-            </Flex>
-            <Text
-              typo="title1"
-              style={{
-                textAlign: 'left',
-                fontSize: window.width < MOBILE ? '4rem' : '',
-              }}
-              tabIndex={isOpen ? 1 : -1}
-              ref={textRef}
-            >
-              {project.name}
-            </Text>
-            <Text
-              typo="number"
-              style={{
-                whiteSpace: window.width >= MOBILE ? 'nowrap' : 'normal',
-              }}
-              tabIndex={isOpen ? 1 : -1}
-            >
-              {project.date}
-            </Text>
-          </Flex>
-          <CardContent
-            direction="column"
-            align="start"
-            justify={isOpen ? 'start' : 'end'}
+            width={isOpen && window.width >= MOBILE ? 'max-content' : '100%'}
             height="100%"
-            gap={window.width >= MOBILE ? 40 : 20}
-            style={{
-              background: isOpen ? colors.primary_white_60 : 'none',
-              padding: isOpen ? '2rem' : '0',
-            }}
+            justify={isOpen ? 'start' : 'space-between'}
+            gap={isOpen || window.width < MOBILE ? 40 : 0}
           >
             <Flex
               direction="column"
               align="start"
-              gap={10}
-              tabIndex={isOpen ? 1 : -1}
+              gap={window.width >= MOBILE ? 20 : 10}
+              style={{ padding: isOpen ? '0 1rem' : '0' }}
             >
-              {isOpen && (
-                <Text typo="body2" color="primary_black">
-                  사용 기술
-                </Text>
-              )}
-              <Tags
-                direction={isOpen || window.width < MOBILE ? 'row' : 'column'}
-                align="start"
-                justify="start"
-                gap={isOpen || window.width < MOBILE ? 8 : 20}
-                style={{ flexWrap: 'wrap' }}
+              <Text
+                typo="title1"
+                style={{
+                  fontSize: window.width < MOBILE ? '4rem' : '',
+                }}
+                tabIndex={isOpen ? 1 : -1}
               >
-                {project.stack.map((stack: string, idx: number) =>
-                  isOpen || window.width < MOBILE ? (
-                    <TagSmall
-                      key={idx}
-                      content={stack}
-                      color="primary_black_60"
-                      background={
-                        isOpen ? 'primary_purple_30' : 'primary_white_30'
-                      }
-                    />
-                  ) : (
-                    <Text
-                      key={idx}
-                      typo={isOpen ? 'body3' : 'number'}
-                      color="primary_black_60"
-                    >
-                      {stack}
-                    </Text>
-                  )
-                )}
-              </Tags>
+                {project.name}
+              </Text>
+              <Text
+                typo="number"
+                style={{
+                  whiteSpace: window.width >= MOBILE ? 'nowrap' : 'normal',
+                }}
+                tabIndex={isOpen ? 1 : -1}
+              >
+                {project.date}
+              </Text>
             </Flex>
-            {isOpen && project.explanation.length ? (
+            <CardContent
+              direction="column"
+              align="start"
+              justify={isOpen ? 'start' : 'end'}
+              height="100%"
+              gap={window.width >= MOBILE ? 40 : 20}
+              style={{
+                background: isOpen ? colors.primary_white_60 : 'none',
+                padding: isOpen ? '2rem' : '0',
+              }}
+            >
               <Flex
-                width={window.width < MOBILE ? '100%' : 'max-content'}
                 direction="column"
                 align="start"
                 gap={10}
                 tabIndex={isOpen ? 1 : -1}
               >
-                <Text typo="body2" color="primary_black">
-                  프로젝트 소개
-                </Text>
-                <Flex direction="column" align="start" gap={4}>
-                  {project.explanation.map(
-                    (explanation: string, idx: number) => (
-                      <Text key={idx} typo="body2" color="primary_black_60">
-                        • {explanation}
+                {isOpen && (
+                  <Text typo="body2" color="primary_black">
+                    사용 기술
+                  </Text>
+                )}
+                <Tags
+                  direction={isOpen || window.width < MOBILE ? 'row' : 'column'}
+                  align="start"
+                  justify="start"
+                  gap={isOpen || window.width < MOBILE ? 8 : 20}
+                  style={{ flexWrap: 'wrap' }}
+                >
+                  {project.stack.map((stack: string, idx: number) =>
+                    isOpen || window.width < MOBILE ? (
+                      <TagSmall
+                        key={idx}
+                        content={stack}
+                        color="primary_black_60"
+                        background={
+                          isOpen ? 'primary_purple_30' : 'primary_white_30'
+                        }
+                      />
+                    ) : (
+                      <Text
+                        key={idx}
+                        typo={isOpen ? 'body3' : 'number'}
+                        color="primary_black_60"
+                      >
+                        {stack}
                       </Text>
                     )
                   )}
-                </Flex>
+                </Tags>
               </Flex>
-            ) : (
-              <></>
-            )}
-            {isOpen && project.role && (
-              <Flex
-                width={window.width >= MOBILE ? 'max-content' : '100%'}
-                direction="column"
-                align="start"
-                gap={10}
-                tabIndex={isOpen ? 1 : -1}
-              >
-                <Text typo="body2" color="primary_black">
-                  수행 역할
-                </Text>
-                <Flex direction="column" align="start" gap={4}>
-                  {project.role.map((role: string, idx: number) => (
-                    <Text key={idx} typo="body2" color="primary_black_60">
-                      • {role}
-                    </Text>
-                  ))}
+              {isOpen && project.explanation.length ? (
+                <Flex
+                  width={window.width < MOBILE ? '100%' : 'max-content'}
+                  direction="column"
+                  align="start"
+                  gap={10}
+                  tabIndex={isOpen ? 1 : -1}
+                >
+                  <Text typo="body2" color="primary_black">
+                    프로젝트 소개
+                  </Text>
+                  <Flex direction="column" align="start" gap={4}>
+                    {project.explanation.map(
+                      (explanation: string, idx: number) => (
+                        <Text key={idx} typo="body2" color="primary_black_60">
+                          • {explanation}
+                        </Text>
+                      )
+                    )}
+                  </Flex>
                 </Flex>
-              </Flex>
-            )}
-          </CardContent>
-        </Flex>
-        {isOpen && (
-          <ImageWrapper tabIndex={isOpen ? 1 : -1}>
-            <NextImage
-              src={project.thumnail}
-              alt={project.name}
-              borderRadius="2rem"
-            />
-          </ImageWrapper>
-        )}
+              ) : (
+                <></>
+              )}
+              {isOpen && project.role && (
+                <Flex
+                  width={window.width >= MOBILE ? 'max-content' : '100%'}
+                  direction="column"
+                  align="start"
+                  gap={10}
+                  tabIndex={isOpen ? 1 : -1}
+                >
+                  <Text typo="body2" color="primary_black">
+                    수행 역할
+                  </Text>
+                  <Flex direction="column" align="start" gap={4}>
+                    {project.role.map((role: string, idx: number) => (
+                      <Text key={idx} typo="body2" color="primary_black_60">
+                        • {role}
+                      </Text>
+                    ))}
+                  </Flex>
+                </Flex>
+              )}
+            </CardContent>
+          </Flex>
+          {isOpen && (
+            <ImageWrapper ref={lastRef} tabIndex={isOpen ? 1 : -1}>
+              <NextImage
+                src={project.thumnail}
+                alt={project.name}
+                borderRadius="2rem"
+              />
+            </ImageWrapper>
+          )}
+        </CardContainer>
       </CardWrapper>
       {isOpen && <Backdrop />}
     </>
@@ -251,8 +272,7 @@ const CardWrapper = styled(motion.button)`
   left: 0;
 
   display: flex;
-  justify-content: space-between;
-  align-items: start;
+  flex-direction: column;
 
   color: ${colors.primary_yellow};
 
@@ -279,6 +299,8 @@ const CardWrapper = styled(motion.button)`
     overflow-y: scroll;
   }
 `;
+
+const CardContainer = styled(Flex)``;
 
 const CardContent = styled(Flex)`
   max-height: max-content;
