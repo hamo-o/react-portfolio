abf4047c41ee6231a793bc5b5597bb82a347239a
hamo-o <yheel369@gmail.com>
2024-01-16 20:55:36 +0900

    fix: font link 태그

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,6 +4,14 @@ export default function Head() {
   return (
     <head>
       <title>HAM</title>
+
+      <link rel="preload" href="https://fonts.cdnfonts.com/css/nats" />
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard-dynamic-subset.min.css"
+      />
+      <link href="https://fonts.cdnfonts.com/css/zen-dots" rel="stylesheet" />
+
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content="FE 개발자 HAM 이현영입니다." />
 
@@ -23,14 +31,6 @@ export default function Head() {
         content="https://www.hamo-o.com/images/thumbnail.png"
       />
       <meta name="twitter:domain" content="https://www.hamo-o.com/" />
-
-      <link rel="stylesheet" href="https://fonts.cdnfonts.com/css/nats" />
-      <link
-        rel="stylesheet"
-        as="style"
-        href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard-dynamic-subset.min.css"
-      />
-      <link href="https://fonts.cdnfonts.com/css/zen-dots" rel="stylesheet" />
     </head>
   );
 }
